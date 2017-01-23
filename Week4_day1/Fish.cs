aea895ae8b473d41657f98b3626ec2aba96305dc
Your Name <you@example.com>
2017-01-23 14:15:21 -0500

    Added in the fish class.

---
+++ Week4_day1/Fish.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4_day1
+{
+    class Fish : Animal
+    {
+        private int fins;
+
+        private bool scales;
+        private bool teeth;
+        private string waterType;
+
+        public Fish(int fins, bool scales, bool teeth, string waterType)
+        {
+            this.fins = fins;
+            this.scales = scales;
+            this.teeth = teeth;
+            this.waterType = waterType;
+        }
+
+    }
+}
