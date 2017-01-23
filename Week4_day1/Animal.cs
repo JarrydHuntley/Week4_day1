1347413d587363d772103770269d22c82e7e0ce4
Your Name <you@example.com>
2017-01-23 11:32:23 -0500

    Added in some fields.

---
+++ Week4_day1/Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4_day1
+{
+    class Animal
+    {
+        private int legs;
+        private int size;
+        private int weight;
+
+        private string color;
+
+        private bool isGoodToEat;
+
+
+        public Animal()
+        {
+
+        }
+
+    }
+}
