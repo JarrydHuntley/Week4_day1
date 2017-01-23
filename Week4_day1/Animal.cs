712887e810dbdf631ff542a0e1e6095ed66eb8a9
Your Name <you@example.com>
2017-01-23 11:37:23 -0500

    Added in some constructors.

--- Week4_day1/Animal.cs
+++ Week4_day1/Animal.cs
@@ -15,11 +15,26 @@ namespace Week4_day1
         private string color;
 
         private bool isGoodToEat;
-
-
-        public Animal()
+        
+        public Animal(int legs, string color)
         {
+            this.legs = legs;
+            this.color = color;
+        }
 
+        public Animal(int legs, int size, int weight)
+        {
+            this.legs = legs;
+            this.size = size;
+            this.weight = weight;
+        }
+
+        public Animal(int legs, int size, int weight, string color)
+        {
+            this.legs = legs;
+            this.size = size;
+            this.weight = weight;
+            this.color = color;
         }
 
     }
